82954bf1ed185a4e4642084581c84b9a039753c1
PIERRATCharly <31919291+PIERRATCharly@users.noreply.github.com>
2021-04-29 21:47:37 +0200

    Create MongoDBConnexion

---
+++ src/main/java/model/MongoDBConnexion.java
@@ -0,0 +1,16 @@
+package model;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoDBConnexion {
+
+    private MongoClient	  mongoClient;
+    private MongoDatabase database;
+
+    public MongoDBConnexion() {
+	mongoClient = MongoClients.create();
+	database = mongoClient.getDatabase("bibliotheque");
+    }
+}
