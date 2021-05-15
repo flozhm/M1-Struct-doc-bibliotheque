fcb81cc2b566821b68c778677f8013709072212a
PIERRATCharly <31919291+PIERRATCharly@users.noreply.github.com>
2021-05-15 09:58:22 +0200

    Ajout etoiles

--- src/main/java/bibliotheque/App.java
+++ src/main/java/bibliotheque/App.java
@@ -1,19 +1,12 @@
 package bibliotheque;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.Commentaire;
-import model.FormationUtilisateur;
-import model.MAJBase;
-import model.Role;
-import model.Utilisateur;
 
 /**
  * JavaFX App
@@ -40,37 +33,39 @@ public class App extends Application {
 
     public static void main(String[] args) {
 
-    	//MAJBase.viderBDD();//On vide la BDD
-    	
-    	//File[] fichiers = null;
-		//fichiers = MAJBase.recupFichiers(); // On récupère les fichiers
-    	
-    	//Insertion commentaire test
-    	//Commentaire com = new Commentaire("Oeuvre", "loginFZ58", "2011-03-16", 9.4, "blabla");
-        //MAJBase.insererCommentaireEnBase(com);
-    	
-    	//Insertion utilisateur test
-    	/*ArrayList<String> universiteRattachement = new ArrayList<String>();
-    	universiteRattachement.add("IDMC");
-    	universiteRattachement.add("DME");
-    	ArrayList<FormationUtilisateur> formation1 = new ArrayList<FormationUtilisateur>();
-    	formation1.add(new FormationUtilisateur("LicenceMIAGE",2016,2019));
-    	formation1.add(new FormationUtilisateur("MasterMIAGE",2019,2021));
-    	Utilisateur user = new Utilisateur("Pierrat", "Charly", universiteRattachement, formation1, Role.Etudiant);
-        MAJBase.insererUtilisateurEnBase(user);*/
-    	
-    	//Insertion formation test
-        
-    
-        /*for (int i = 0; i < fichiers.length; i++) { // On boucle sur les fichiers du répertoire
-			MAJBase.lireFichier(fichiers[i]);
-			// String fileName = fichiers[i].getName(); // On récupère le nom du fichier
-			// MAJBase.importerOeuvreEnBase(fichiers[i]); //On insère les données des
-			// fichiers un par un dans la BDD
-		}*/
+	// MAJBase.viderBDD();//On vide la BDD
 
+	// File[] fichiers = null;
+	// fichiers = MAJBase.recupFichiers(); // On récupère les fichiers
 
-    	//launch(); // On lance l'application*/
-    
+	// Insertion commentaire test
+	// Commentaire com = new Commentaire("Oeuvre", "loginFZ58", "2011-03-16", 9.4,
+	// "blabla");
+	// MAJBase.insererCommentaireEnBase(com);
+
+	// Insertion utilisateur test
+	/*
+	 * ArrayList<String> universiteRattachement = new ArrayList<String>();
+	 * universiteRattachement.add("IDMC"); universiteRattachement.add("DME");
+	 * ArrayList<FormationUtilisateur> formation1 = new
+	 * ArrayList<FormationUtilisateur>(); formation1.add(new
+	 * FormationUtilisateur("LicenceMIAGE",2016,2019)); formation1.add(new
+	 * FormationUtilisateur("MasterMIAGE",2019,2021)); Utilisateur user = new
+	 * Utilisateur("Pierrat", "Charly", universiteRattachement, formation1,
+	 * Role.Etudiant); MAJBase.insererUtilisateurEnBase(user);
+	 */
+
+	// Insertion formation test
+
+	/*
+	 * for (int i = 0; i < fichiers.length; i++) { // On boucle sur les fichiers du
+	 * répertoire MAJBase.lireFichier(fichiers[i]); // String fileName =
+	 * fichiers[i].getName(); // On récupère le nom du fichier //
+	 * MAJBase.importerOeuvreEnBase(fichiers[i]); //On insère les données des //
+	 * fichiers un par un dans la BDD }
+	 */
+
+	launch(); // On lance l'application*/
+
     }
 }
