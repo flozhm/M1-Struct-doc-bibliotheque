fd22468e29dd51faa3259c82f109e684079b8fc8
PIERRATCharly <31919291+PIERRATCharly@users.noreply.github.com>
2021-05-18 07:40:36 +0200

    Amélioration vitesse

--- src/main/java/bibliotheque/App.java
+++ src/main/java/bibliotheque/App.java
@@ -3,7 +3,6 @@ package bibliotheque;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Logger;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,36 +15,37 @@ import model.MAJBase;
  */
 public class App extends Application {
 
-	private static final Logger LOG = Logger.getLogger(App.class.getName());
+    private static final Logger LOG = Logger.getLogger(App.class.getName());
 
-	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Bibliotheque");
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("bibliotheque.fxml"));
-			Scene scene = new Scene(root);
-			primaryStage.setScene(scene);
-			primaryStage.setMaximized(true);
-			primaryStage.show();
-		} catch (IOException e) {
-			e.printStackTrace();
-			LOG.severe("Erreur");
-		}
+    @Override
+    public void start(Stage primaryStage) {
+	primaryStage.setTitle("Bibliotheque");
+	try {
+	    Parent root = FXMLLoader.load(getClass().getResource("bibliotheque.fxml"));
+	    Scene scene = new Scene(root);
+	    primaryStage.setScene(scene);
+	    primaryStage.setMaximized(true);
+	    primaryStage.show();
+	}
+	catch (IOException e) {
+	    e.printStackTrace();
+	    LOG.severe("Erreur");
+	}
+    }
+
+    public static void main(String[] args) {
+
+	MAJBase.viderBDD();// On vide la BDD
+
+	File[] fichiers = null;
+	fichiers = MAJBase.recupFichiers(); // On récupère les fichiers
+
+	for (int i = 0; i < fichiers.length; i++) {
+	    MAJBase.lireFichier(fichiers[i]);
 	}
 
-	public static void main(String[] args) {
+	launch(); // On lance l'application*/
 
-		// MAJBase.viderBDD();// On vide la BDD
-
-		File[] fichiers = null;
-		fichiers = MAJBase.recupFichiers(); // On récupère les fichiers
-
-		for (int i = 0; i < fichiers.length; i++) {
-			MAJBase.lireFichier(fichiers[i]);
-		}
-
-		launch(); // On lance l'application*/
-
-	}
+    }
 
 }
--- src/main/java/model/Oeuvre.java
+++ src/main/java/model/Oeuvre.java
@@ -6,135 +6,134 @@ import java.util.List;
 
 public class Oeuvre {
 
-	private String titre;
-	private List<Auteur> auteurs;
-	private int nbPage;
-	private LocalDate datePubli;
-	private LocalDate dateDerCommentaire = null; // Pas dans la BDD (calculée)
-	private String theme;
-	private Role role;
-	private String contenu;
-	private String note = "Non notée"; // Pas dans la BDD (calculée)
+    private String	 titre;
+    private List<Auteur> auteurs;
+    private int		 nbPage;
+    private LocalDate	 datePubli;
+    private LocalDate	 dateDerCommentaire = null;	   // Pas dans la BDD (calculée)
+    private String	 theme;
+    private Role	 role;
+    private String	 contenu;
+    private String	 note		    = "Non notée"; // Pas dans la BDD (calculée)
 
-	public Oeuvre() {
-		this.auteurs = new ArrayList<Auteur>();
-		calculerNote();
-		calculerDateDerCommentaire();
-	}
+    public Oeuvre() {
+	this.auteurs = new ArrayList<Auteur>();
+	calculerNote();
+	calculerDateDerCommentaire();
+    }
 
-	// Pour les tests
-	public Oeuvre(String titre, List<Auteur> auteurs, int nbPage, LocalDate datePubli, Role role, String theme,
-			String contenu, Boolean b) {
-		this.titre = titre;
-		this.auteurs = auteurs;
-		this.nbPage = nbPage;
-		this.datePubli = datePubli;
-		this.role = role;
-		this.theme = theme;
-		this.contenu = contenu;
-	}
+    // Pour les tests
+    public Oeuvre(String titre, List<Auteur> auteurs, int nbPage, LocalDate datePubli, Role role, String theme,
+	    String contenu, Boolean b) {
+	this.titre = titre;
+	this.auteurs = auteurs;
+	this.nbPage = nbPage;
+	this.datePubli = datePubli;
+	this.role = role;
+	this.theme = theme;
+	this.contenu = contenu;
+    }
 
-	public Oeuvre(String titre, List<Auteur> auteurs, int nbPage, LocalDate datePubli, Role role, String theme,
-			String contenu) {
-		this.titre = titre;
-		this.auteurs = auteurs;
-		this.nbPage = nbPage;
-		this.datePubli = datePubli;
-		this.role = role;
-		this.theme = theme;
-		this.contenu = contenu;
-		calculerNote();
-		calculerDateDerCommentaire();
-	}
+    public Oeuvre(String titre, List<Auteur> auteurs, int nbPage, LocalDate datePubli, Role role, String theme,
+	    String contenu) {
+	this.titre = titre;
+	this.auteurs = auteurs;
+	this.nbPage = nbPage;
+	this.datePubli = datePubli;
+	this.role = role;
+	this.theme = theme;
+	this.contenu = contenu;
+	calculerNote();
+	calculerDateDerCommentaire();
+    }
 
-	@Override
-	public String toString() {
-		return "Oeuvre\n- titre: " + titre + "\nauteurs: " + auteurs + "\nnbPage: " + nbPage + "\ndatePubli: "
-				+ datePubli + "\nTheme: " + theme + "\nrole= " + role + "\ncontenu=" + contenu + "\n------------";
-	}
+    @Override
+    public String toString() {
+	return "Oeuvre\n- titre: " + titre + "\nauteurs: " + auteurs + "\nnbPage: " + nbPage + "\ndatePubli: "
+		+ datePubli + "\nTheme: " + theme + "\nrole= " + role + "\ncontenu=" + contenu + "\n------------";
+    }
 
-	public String getTitre() {
-		return titre;
-	}
+    public String getTitre() {
+	return titre;
+    }
 
-	public void setTitre(String titre) {
-		this.titre = titre;
-	}
+    public void setTitre(String titre) {
+	this.titre = titre;
+    }
 
-	public List<Auteur> getAuteurs() {
-		return auteurs;
-	}
+    public List<Auteur> getAuteurs() {
+	return auteurs;
+    }
 
-	public void setAuteurs(List<Auteur> auteurs) {
-		this.auteurs = auteurs;
-	}
+    public void setAuteurs(List<Auteur> auteurs) {
+	this.auteurs = auteurs;
+    }
 
-	public int getNbPage() {
-		return nbPage;
-	}
+    public int getNbPage() {
+	return nbPage;
+    }
 
-	public void setNbPage(int nbPage) {
-		this.nbPage = nbPage;
-	}
+    public void setNbPage(int nbPage) {
+	this.nbPage = nbPage;
+    }
 
-	public LocalDate getDatePubli() {
-		return datePubli;
-	}
+    public LocalDate getDatePubli() {
+	return datePubli;
+    }
 
-	public void setDatePubli(LocalDate datePubli) {
-		this.datePubli = datePubli;
-	}
+    public void setDatePubli(LocalDate datePubli) {
+	this.datePubli = datePubli;
+    }
 
-	public LocalDate getDateDerCommentaire() {
-		calculerDateDerCommentaire();
-		return dateDerCommentaire;
-	}
+    public LocalDate getDateDerCommentaire() {
+	return dateDerCommentaire;
+    }
 
-	public void calculerDateDerCommentaire() {
-		MongoDBConnexion mdb = new MongoDBConnexion();
-		LocalDate newDateDerCommentaire = mdb.getDateDerCommentaire(this);
-		if (newDateDerCommentaire != null) {
-			dateDerCommentaire = newDateDerCommentaire;
-		}
+    public void calculerDateDerCommentaire() {
+	MongoDBConnexion mdb = new MongoDBConnexion();
+	LocalDate newDateDerCommentaire = mdb.getDateDerCommentaire(this);
+	if (newDateDerCommentaire != null) {
+	    dateDerCommentaire = newDateDerCommentaire;
 	}
+    }
 
-	public String getTheme() {
-		return theme;
-	}
+    public String getTheme() {
+	return theme;
+    }
 
-	public void setTheme(String theme) {
-		this.theme = theme;
-	}
+    public void setTheme(String theme) {
+	this.theme = theme;
+    }
 
-	public Role getRole() {
-		return role;
-	}
+    public Role getRole() {
+	return role;
+    }
 
-	public void setRole(Role role) {
-		this.role = role;
-	}
+    public void setRole(Role role) {
+	this.role = role;
+    }
 
-	public String getContenu() {
-		return contenu;
-	}
+    public String getContenu() {
+	return contenu;
+    }
 
-	public void setContenu(String contenu) {
-		this.contenu = contenu;
-	}
+    public void setContenu(String contenu) {
+	this.contenu = contenu;
+    }
 
-	public String getNote() {
-		calculerNote();
-		return note;
-	}
+    public String getNote() {
+	return note;
+    }
 
-	public void calculerNote() {
-		MongoDBConnexion mdb = new MongoDBConnexion();
-		Double newNote = mdb.getNote(this);
-		if (Double.isNaN(newNote)) {
-			note = "Non notée";
-		} else {
-			note = newNote.toString();
-		}
+    public void calculerNote() {
+	MongoDBConnexion mdb = new MongoDBConnexion();
+	Double newNote = mdb.getNote(this);
+	if (Double.isNaN(newNote)) {
+	    note = "Non notée";
 	}
+	else {
+	    note = newNote.toString();
+	}
+    }
 
 }
