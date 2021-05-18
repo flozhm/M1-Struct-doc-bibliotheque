f2ec39e081cbd2290d46a8686e47cd6eb4084611
flzhm <45990224+flozhm@users.noreply.github.com>
2021-05-18 12:06:10 +0200

    MAJ Main

--- src/main/java/bibliotheque/App.java
+++ src/main/java/bibliotheque/App.java
@@ -1,50 +1,50 @@
 package bibliotheque;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.logging.Logger;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.MAJBase;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    private static final Logger LOG = Logger.getLogger(App.class.getName());
+	private static final Logger LOG = Logger.getLogger(App.class.getName());
 
-    @Override
-    public void start(Stage primaryStage) {
-	primaryStage.setTitle("Bibliotheque");
-	try {
-	    Parent root = FXMLLoader.load(getClass().getResource("bibliotheque.fxml"));
-	    Scene scene = new Scene(root);
-	    primaryStage.setScene(scene);
-	    primaryStage.setMaximized(true);
-	    primaryStage.show();
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Bibliotheque");
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("bibliotheque.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setMaximized(true);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+			LOG.severe("Erreur");
+		}
 	}
-	catch (IOException e) {
-	    e.printStackTrace();
-	    LOG.severe("Erreur");
+
+	public static void main(String[] args) {
+
+		// MAJBase.viderBDD();// On vide la BDD
+
+		File[] fichiers = MAJBase.recupFichiers(); // On récupère les fichiers
+
+		for (int i = 0; i < fichiers.length; i++) {
+			MAJBase.lireFichier(fichiers[i]);
+		}
+
+		launch(); // On lance l'application*/
+
 	}
-    }
-
-    public static void main(String[] args) {
-
-	/*
-	 * MAJBase.viderBDD();// On vide la BDD
-	 * 
-	 * File[] fichiers = null; fichiers = MAJBase.recupFichiers(); // On récupère
-	 * les fichiers
-	 * 
-	 * for (int i = 0; i < fichiers.length; i++) { MAJBase.lireFichier(fichiers[i]);
-	 * }
-	 */
-
-	launch(); // On lance l'application*/
-
-    }
 
 }
