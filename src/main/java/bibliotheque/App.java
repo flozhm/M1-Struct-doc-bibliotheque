ec9eee7909df6de84c6b4c1eba1a2038dea467a2
PIERRATCharly <31919291+PIERRATCharly@users.noreply.github.com>
2021-05-18 09:39:47 +0200

    Lancement application seulement

--- src/main/java/bibliotheque/App.java
+++ src/main/java/bibliotheque/App.java
@@ -1,6 +1,5 @@
 package bibliotheque;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.logging.Logger;
 import javafx.application.Application;
@@ -8,7 +7,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.MAJBase;
 
 /**
  * JavaFX App
@@ -35,14 +33,15 @@ public class App extends Application {
 
     public static void main(String[] args) {
 
-	MAJBase.viderBDD();// On vide la BDD
-
-	File[] fichiers = null;
-	fichiers = MAJBase.recupFichiers(); // On récupère les fichiers
-
-	for (int i = 0; i < fichiers.length; i++) {
-	    MAJBase.lireFichier(fichiers[i]);
-	}
+	/*
+	 * MAJBase.viderBDD();// On vide la BDD
+	 * 
+	 * File[] fichiers = null; fichiers = MAJBase.recupFichiers(); // On récupère
+	 * les fichiers
+	 * 
+	 * for (int i = 0; i < fichiers.length; i++) { MAJBase.lireFichier(fichiers[i]);
+	 * }
+	 */
 
 	launch(); // On lance l'application*/
 
