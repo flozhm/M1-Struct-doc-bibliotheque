f3e550247b0c88de8d35825897f6658f9ec2bdb7
flzhm <45990224+flozhm@users.noreply.github.com>
2021-05-18 23:31:46 +0200

    MAJ main

--- src/main/java/bibliotheque/App.java
+++ src/main/java/bibliotheque/App.java
@@ -1,5 +1,6 @@
 package bibliotheque;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.logging.Logger;
 
@@ -8,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.MAJBase;
 
 /**
  * JavaFX App
@@ -35,14 +37,13 @@ public class App extends Application {
 
 	public static void main(String[] args) {
 
-		/*
-		 * MAJBase.viderBDD();// On vide la BDD
-		 * 
-		 * File[] fichiers = MAJBase.recupFichiers(); // On récupère les fichiers
-		 * 
-		 * for (int i = 0; i < fichiers.length; i++) { MAJBase.lireFichier(fichiers[i]);
-		 * }
-		 */
+		MAJBase.viderBDD();// On vide la BDD
+
+		File[] fichiers = MAJBase.recupFichiers(); // On récupère les fichiers
+
+		for (int i = 0; i < fichiers.length; i++) {
+			MAJBase.lireFichier(fichiers[i]);
+		}
 
 		launch(); // On lance l'application*/
 
